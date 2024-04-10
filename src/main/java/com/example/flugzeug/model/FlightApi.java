4ac38eb6e8d1f83eaff2ff23fbbda26f5a0e39a7
NikitaDeveloper <nik0609tak@gmail.com>
2024-04-10 14:53:29 +0200

    Minor improvements in Mapping

---
+++ src/main/java/com/example/flugzeug/model/FlightApi.java
@@ -0,0 +1,25 @@
+package com.example.flugzeug.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+public class FlightApi
+{
+    private Long id;
+    private String name;
+    private List<SitplaceApi> seats;
+
+    public FlightApi(Long id, String name, List<Sitplace> seats)
+    {
+        this.id = id;
+        this.name = name;
+        this.seats = seats.stream()
+                .map(Sitplace::toApi)
+                .collect(Collectors.toList());
+    }
+}
